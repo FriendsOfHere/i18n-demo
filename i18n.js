d68bbbbafa2ce4a49609a242902cf463653f77d0
lifesign <leocloudy@gmail.com>
2020-02-23 17:39:17 +0800

    poc make it work

---
+++ i18n.js
@@ -0,0 +1,41 @@
+function i18n(template, ...expression) {
+    let info = i18n.db[i18n.locale][template.join('\x01')];
+    if (typeof info == "undefined") {
+        let output = ``
+        for (const [i, val] of expression.entries()) {
+            output += template[i] + val
+        }
+        output += template[template.length - 1]
+        return output
+    }
+
+  for (var
+    out = [info.t[0]],
+    i = 1, length = info.t.length; i < length; i++
+  ) out[i] = arguments[1 + info.v[i - 1]] + info.t[i];
+  return out.join('');
+}
+
+i18n.set = locale => (tCurrent, ...rCurrent) => {
+  const key = tCurrent.join('\x01');
+  let db = i18n.db[locale] || (i18n.db[locale] = {});
+  db[key] = {
+    t: tCurrent.slice(),
+    v: rCurrent.map((value, i) => i)
+  };
+  const config = {
+    for: other => (tOther, ...rOther) => {
+      db = i18n.db[other] || (i18n.db[other] = {});
+      db[key] = {
+        t: tOther.slice(),
+        v: rOther.map((value, i) => rCurrent.indexOf(value))
+      };
+      return config;
+    }
+  };
+  return config;
+};
+
+i18n.db = {}
+
+module.exports = i18n
--- index.js
+++ index.js
@@ -9,24 +9,80 @@
 
 const _ = require("lodash")
 const net = require("net")
+const cache = require('cache')
+const pref = require('pref')
+let i18nTool = require("./i18n.js")
+const langMap = ['en','zh']
 
-function updateData() {
+function loadActualLang(locale) {
+    //Pref
+    i18nTool.set('en')`Interface Language`
+    .for('zh')`界面语言`
+    i18nTool.set('en')`Food`
+    .for('zh')`食物`
+    i18nTool.set('en')`Banana`
+    .for('zh')`香蕉`
+    i18nTool.set('en')`Apple`
+    .for('zh')`苹果`
 
+    //notification
+    i18nTool.set('en')`Notification: Success`
+    .for('zh')`通知：成功`
+    i18nTool.set('en')`Please Restart`
+    .for('zh')`请重启生效`
 
+    i18nTool.locale = locale
+}
 
+function changeLang() {
+    // loadLangConfig(langMap[_.toSafeInteger(pref.get('language'))])
+    let actualLang = _.toSafeInteger(cache.get('currentLang'))
+    let prefLang = _.toSafeInteger(pref.get('prefLang'))
+    console.log(`actualLang: ${actualLang} prefLang: ${prefLang}`)
+    loadActualLang(langMap[prefLang])
+    //debug i18n db
+    console.log(JSON.stringify(i18nTool.db))
 
+    let i18nTitle = i18nTool`Interface Language`
+    let i18nFood = i18nTool`Food`
+    let i18nBanana = i18nTool`Banana`
+    let i18nApple = i18nTool`Apple`
+    console.log(`${i18nTitle}:${i18nFood}:${i18nBanana}:${i18nApple}`)
 
+    //rewrite current config, interface language will effect next time
+    if (actualLang != prefLang) {
+        console.log("Begin rewrite config.json")
+        here.exec(`
+cp config.default.json config.json;
+perl -pi -e 's/{{i18nTitle}}/${i18nTitle}/g' ./config.json;
+perl -pi -e 's/{{i18nDefault}}/${prefLang}/g' ./config.json;
+perl -pi -e 's/{{i18nFood}}/${i18nFood}/g' ./config.json;
+perl -pi -e 's/{{i18nBanana}}/${i18nBanana}/g' ./config.json;
+perl -pi -e 's/{{i18nApple}}/${i18nApple}/g' ./config.json;
+`)
+                .then((output) => {
+                    console.log(output)
+                    cache.set('currentLang', prefLang)
+                    //generate notification
+                    here.systemNotification(i18nTool`Notification: Success`, i18nTool`Please Restart`)
+                })
+    } else {
+        console.log("Nothing Changed")
+    }
 }
 
 here.onLoad(() => {
-    updateData()
+    
+    //切换语言 Demo
+    changeLang()
+
     // Update every 2 hours
-    setInterval(updateData, 2*3600*1000);
+    setInterval(changeLang, 2*3600*1000);
 })
 
 net.onChange((type) => {
     console.log("Connection type changed:", type)
     if (net.isReachable()) {
-        updateData()
+        changeLang()
     }
 })
