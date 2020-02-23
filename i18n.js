cc3518319b461629621d0dfbc39040b5c842bc1e
lifesign <leocloudy@gmail.com>
2020-02-23 17:42:01 +0800

    format

--- i18n.js
+++ i18n.js
@@ -9,31 +9,30 @@ function i18n(template, ...expression) {
         return output
     }
 
-  for (var
-    out = [info.t[0]],
-    i = 1, length = info.t.length; i < length; i++
-  ) out[i] = arguments[1 + info.v[i - 1]] + info.t[i];
-  return out.join('');
+    for (var
+            out = [info.t[0]],
+            i = 1, length = info.t.length; i < length; i++) out[i] = arguments[1 + info.v[i - 1]] + info.t[i];
+    return out.join('');
 }
 
 i18n.set = locale => (tCurrent, ...rCurrent) => {
-  const key = tCurrent.join('\x01');
-  let db = i18n.db[locale] || (i18n.db[locale] = {});
-  db[key] = {
-    t: tCurrent.slice(),
-    v: rCurrent.map((value, i) => i)
-  };
-  const config = {
-    for: other => (tOther, ...rOther) => {
-      db = i18n.db[other] || (i18n.db[other] = {});
-      db[key] = {
-        t: tOther.slice(),
-        v: rOther.map((value, i) => rCurrent.indexOf(value))
-      };
-      return config;
-    }
-  };
-  return config;
+    const key = tCurrent.join('\x01');
+    let db = i18n.db[locale] || (i18n.db[locale] = {});
+    db[key] = {
+        t: tCurrent.slice(),
+        v: rCurrent.map((value, i) => i)
+    };
+    const config = {
+        for: other => (tOther, ...rOther) => {
+            db = i18n.db[other] || (i18n.db[other] = {});
+            db[key] = {
+                t: tOther.slice(),
+                v: rOther.map((value, i) => rCurrent.indexOf(value))
+            };
+            return config;
+        }
+    };
+    return config;
 };
 
 i18n.db = {}
